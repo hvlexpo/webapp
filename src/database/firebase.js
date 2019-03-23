678f0296fc0d1a75e7747485141fbea46ec626ce
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-03-23 17:32:45 +0100

    :sparkles: Firebase config

---
+++ src/database/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+	apiKey: "AIzaSyB9cjEEfM-8GHB5H0NVUsb8dZ031hA0TAk",
+	authDomain: "hvl-expo.firebaseapp.com",
+	databaseURL: "https://hvl-expo.firebaseio.com",
+	projectId: "hvl-expo",
+	storageBucket: "hvl-expo.appspot.com",
+	messagingSenderId: "54626777503"
+}
+
+firebase.initializeApp(config)
+
+export default firebase
